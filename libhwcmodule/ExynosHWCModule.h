e21944ff55e1aec51d70139513767c51dc22b7d0
Salih Çağlar <111198755+salih4187@users.noreply.github.com>
2025-10-12 14:06:18 +0300

    Update for virtual display support

--- libhwcmodule/ExynosHWCModule.h
+++ libhwcmodule/ExynosHWCModule.h
@@ -87,13 +87,21 @@ const size_t FIMD_EXT_MPP_IDX = 0;
 /* HDMI_GSC_IDX is not used but added for build issue */
 const size_t HDMI_GSC_IDX = 2;
 const size_t HDMI_EXT_MPP_IDX = 2;
-const size_t WFD_GSC_IDX = 1;
+#ifdef USES_VIRTUAL_DISPLAY
+const size_t WFD_GSC_IDX = 3;
+#else
+const size_t WFD_GSC_DRM_IDX = 3;
+#endif
 const size_t WFD_EXT_MPP_IDX = 1;
 
 const size_t FIMD_GSC_SEC_IDX = 1;
 
-const int FIMD_GSC_USAGE_IDX[] = {FIMD_GSC_IDX};
-const int AVAILABLE_GSC_UNITS[] = { 0, 1};
+const int FIMD_GSC_USAGE_IDX[] = {FIMD_GSC_IDX, FIMD_GSC_SEC_IDX};
+#ifdef USES_VIRTUAL_DISPLAY
+const int AVAILABLE_GSC_UNITS[] = { 0, 1, 1, 1 };
+#else
+const int AVAILABLE_GSC_UNITS[] = { 0, 1, 1, 5 };
+#endif
 
 #define MPP_VG          0
 #define MPP_VGR         2
