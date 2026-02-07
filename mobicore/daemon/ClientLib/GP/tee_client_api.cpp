a42ca7517995f8ff382a6b8c72120c54cc060438
Salih Çağlar <salihcaglar4187@gmail.com>
2026-02-07 15:22:59 +0300

    Revert "mobicore: Convert strcpy to memcpy "
    This reverts commit 200aa9eef0a379aa4ffb7571a0c17766b3c07daa.

--- mobicore/daemon/ClientLib/GP/tee_client_api.cpp
+++ mobicore/daemon/ClientLib/GP/tee_client_api.cpp
@@ -236,7 +236,7 @@ static TEEC_Result _TEEC_SetupOperation(
     }
 
     //Copy version indicator field
-    memcpy(tci->header, "TCIGP000", 8);
+    strcpy(tci->header, "TCIGP000");
 
     // Fill in invalid values for secure world to overwrite
     tci->returnStatus = 0;
